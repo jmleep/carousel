baa9f5f5d982978f5058b87c93d0cbc6b03cb88d
Jordan Leeper <j.m.leep@gmail.com>
2024-02-19 21:39:57 -0600

    fix: adds ability to provide callback functions for state changes

--- README.md
+++ README.md
@@ -12,36 +12,66 @@ Create a carousel object with the desired iterable items.
 
 ```ts
 // with default speed of 1000ms
-const carousel = new Carousel(["item1", "item2", "item3"]);
+const carousel = new Carousel(["item1", "item2", "item3"])
+
+function onActiveItemChange(activeItem: string) {
+  console.log(activeItem)
+}
+
+function onPlayingStatechange(isPlaying: boolean) {
+  console.log(isPlaying)
+}
 
 // with 5000ms speed
-const carousel2 = new Carousel(["item1", "item2", "item3"], 5000);
+const carousel2 = new Carousel(
+  ["item1", "item2", "item3"],
+  onActiveItemChange,
+  onPlayingStateChange,
+  5000
+)
 ```
 
 Use the play/pause functions to start and stop the carousel
 
 ```ts
-carousel.play();
+carousel.play()
 
-carousel.pause();
+carousel.pause()
 ```
 
-Access the active item by using the activeItem property
+Access the active item by using the activeItem property or by providing a callback function to the Carousel to be notified of changes to the active item. Access the playing state (boolean) by using the isPlaying property or by providing a callback function to the onPlayingStateChange function.
 
 ```ts
-const active = carousel.activeItem;
+const active = carousel.activeItem
+const isPlaying = carousel.isPlaying
+
+function onActiveItemChange(activeItem: string) {
+  console.log(activeItem)
+}
+
+function onPlayingStatechange(isPlaying: boolean) {
+  console.log(isPlaying)
+}
+
+// with 5000ms speed
+const carousel2 = new Carousel(
+  ["item1", "item2", "item3"],
+  onActiveItemChange,
+  onPlayingStateChange,
+  5000
+)
 ```
 
 Manually move through the carousel items by using the next/previous functions
 
 ```ts
-carousel.next();
+carousel.next()
 
-carousel.previous();
+carousel.previous()
 ```
 
 Update the carousel speed
 
 ```ts
-carousel.setSpeed(2000);
+carousel.setSpeed(2000)
 ```
--- lib/index.ts
+++ lib/index.ts
@@ -1,90 +1,101 @@
 function* carouselGenerator(
   itemsLength: number
 ): Generator<any, number, number> {
-  let i = 0;
+  let i = 0
   while (true) {
-    let input = yield i;
+    let input = yield i
 
     if (input === undefined) {
-      i++;
+      i++
       if (i >= itemsLength) {
-        i = 0;
+        i = 0
       }
     } else {
-      i = input;
+      i = input
     }
   }
 }
 
 class Carousel<T> {
-  private _generator: Generator<any, number, number>;
-  private _interval: number | undefined = undefined;
-  private _activeIndex: number;
-  isPlaying: boolean;
-  activeItem: T;
-  intervalSpeed: number;
-  items: T[];
+  private _generator: Generator<any, number, number>
+  private _interval: number | undefined = undefined
+  private _activeIndex: number
+  isPlaying: boolean
+  activeItem: T
+  intervalSpeed: number
+  items: T[]
+  onActiveItemChange
+  onPlayingStateChange
 
-  constructor(items: T[], intervalSpeed: number = 1000) {
-    this.items = items;
-    this.intervalSpeed = intervalSpeed;
-    this.isPlaying = false;
-    this._activeIndex = 0;
-    this.activeItem = items[this._activeIndex];
-    this._generator = carouselGenerator(items.length);
+  constructor(
+    items: T[],
+    onActiveItemChange: (activeItem: T) => any,
+    onPlayingStateChange: (isPlaying: boolean) => void = () => {},
+    intervalSpeed: number = 1000
+  ) {
+    this.items = items
+    this.intervalSpeed = intervalSpeed
+    this.onActiveItemChange = onActiveItemChange
+    this.onPlayingStateChange = onPlayingStateChange
+    this.isPlaying = false
+    this._activeIndex = 0
+    this.activeItem = items[this._activeIndex]
+    this._generator = carouselGenerator(items.length)
     // trigger the first iteration so that the generator is ready
-    this._generator.next();
+    this._generator.next()
+    this.onActiveItemChange(items[0])
   }
 
   play() {
     if (!this.isPlaying) {
-      this.isPlaying = true;
-      this._startInterval();
+      this.isPlaying = true
+      this.onPlayingStateChange(this.isPlaying)
+      this._startInterval()
     }
   }
 
   pause() {
-    this.isPlaying = false;
-    clearInterval(this._interval);
+    this.isPlaying = false
+    this.onPlayingStateChange(this.isPlaying)
+    window.clearInterval(this._interval)
   }
 
   next() {
-    this._setActiveItem(this._generator.next().value);
+    this._setActiveItem(this._generator.next().value)
   }
 
   previous() {
-    let previousIndex = this._activeIndex - 1;
+    let previousIndex = this._activeIndex - 1
     if (previousIndex < 0) {
-      previousIndex = this.items.length - 1;
+      previousIndex = this.items.length - 1
     }
-    this._setActiveItem(this._generator.next(previousIndex).value);
+
+    this._setActiveItem(this._generator.next(previousIndex).value)
   }
 
   setSpeed(speed: number) {
-    this.intervalSpeed = speed;
+    this.intervalSpeed = speed
     if (this.isPlaying) {
-      this._startInterval();
+      this._startInterval()
     }
   }
 
   getActiveItem() {
-    return this.activeItem;
+    return this.activeItem
   }
 
   private _startInterval() {
-    clearInterval(this._interval);
+    window.clearInterval(this._interval)
     this._interval = window.setInterval(() => {
-      this._setActiveItem(this._generator.next().value);
-    }, this.intervalSpeed);
+      this._setActiveItem(this._generator.next().value)
+    }, this.intervalSpeed)
   }
 
   private _setActiveItem(index: number) {
-    this._activeIndex = index;
-    this.activeItem = this.items[index];
-    dispatchEvent(
-      new CustomEvent("carousel-item-change", { detail: this.activeItem })
-    );
+    this._activeIndex = index
+    this.activeItem = this.items[index]
+    this.onActiveItemChange(this.activeItem)
   }
 }
 
-export default Carousel;
+export default Carousel
